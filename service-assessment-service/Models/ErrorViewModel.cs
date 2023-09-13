a65d61f6a58ccff856e89e9e1d19fcd81b8df757
dfeetenby <David.Feetenby@digital.education.gov.uk>
2023-09-13 09:52:45 +0100

    Create Solution
    Add default MVC Web app

---
+++ service-assessment-service/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace service_assessment_service.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
