e97804b52248ca76466448924a8aad271e101760
Roger Howell <roger.howell@education.gov.uk>
2023-09-18 18:17:44 +0100

    Add sass dependency

--- src/ServiceAssessmentService/ServiceAssessmentService.WebApp/wwwroot/js/site.js
+++ src/ServiceAssessmentService/ServiceAssessmentService.WebApp/wwwroot/js/site.js
@@ -1,18 +1,5 @@
-﻿// JS Dependencies: Popper, Bootstrap & JQuery
-import "@popperjs/core";
-import "bootstrap";
-import "jquery";
-// Using the next two lines is like including partial view _ValidationScriptsPartial.cshtml
-import "jquery-validation";
-import "jquery-validation-unobtrusive";
-
-// CSS Dependencies: Bootstrap
-import "bootstrap/dist/css/bootstrap.css";
-
-// Custom JS imports
-// ... none at the moment
-
-// Custom CSS imports
+﻿// Custom CSS imports
 import "../css/site.scss";
-
+// Custom JS imports
+import "/node_modules/dfe-frontend-alpha/dist/dfefrontend.js";
 console.log("The 'site' bundle has been loaded!");
