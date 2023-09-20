185f08d345f841d47da0edf5ad3bac8c5d9f4815
Roger Howell <roger.howell@education.gov.uk>
2023-09-20 17:58:52 +0100

    Flesh out initial dashboard view and DfE design system template/layout

--- src/ServiceAssessmentService/ServiceAssessmentService.WebApp/Controllers/HomeController.cs
+++ src/ServiceAssessmentService/ServiceAssessmentService.WebApp/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult Index()
+    public IActionResult Dashboard()
     {
         return View();
     }
--- src/ServiceAssessmentService/ServiceAssessmentService.WebApp/Program.cs
+++ src/ServiceAssessmentService/ServiceAssessmentService.WebApp/Program.cs
@@ -22,7 +22,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Dashboard}/{id?}");
 
 app.Run();
 
