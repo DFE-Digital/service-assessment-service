eb638f71fccdfe289d0a47d0a7f302cbeed3c8d4
Roger Howell <roger.howell@education.gov.uk>
2023-10-28 20:11:29 +0100

    Add basic HTML for the entry page into booking a new assessment
    #1

---
+++ src/ServiceAssessmentService/ServiceAssessmentService.WebApp/Controllers/BookController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceAssessmentService.WebApp.Controllers;
+
+public class BookController : Controller
+{
+    private readonly ILogger<BookController> _logger;
+
+    public BookController(ILogger<BookController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
