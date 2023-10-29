1b414a36bb27eb8db00504d9d05d355b59d893fd
Roger Howell <roger.howell@education.gov.uk>
2023-10-29 01:01:04 +0100

    Switch implementation to use govuk-frontend-aspnetcore tag helper library (minimise amount of hand-curated HTML which must be maintained as GDS updates/changes)
    https://github.com/gunndabad/govuk-frontend-aspnetcore/

--- src/ServiceAssessmentService/ServiceAssessmentService.WebApp/Program.cs
+++ src/ServiceAssessmentService/ServiceAssessmentService.WebApp/Program.cs
@@ -1,7 +1,11 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using GovUk.Frontend.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddGovUkFrontend();
 
 var app = builder.Build();
 
