a191e6e1b7387437b0b41be31510dd5dfc84c356
Roger Howell <roger.howell@education.gov.uk>
2024-01-23 18:30:50 +0000

    Add SiteSection attribute to pages and modify navigation to use this when marking navbar items as "active".
    Additionally, a developer-facing information/warning message is displayed on pages where this has not been provided/overridden from the default value.

---
+++ src/ServiceAssessmentService/ServiceAssessmentService.WebApp/Models/SiteSection.cs
@@ -0,0 +1,16 @@
+﻿namespace ServiceAssessmentService.WebApp.Models;
+
+/// <summary>
+/// This enum is used to signify the section of the site a page belongs.
+/// - The navigation bar uses this to determine which (if any) nav item should be highlighted as "active".
+/// </summary>
+internal enum SiteSection
+{
+    Default,
+    PublicStatic,
+    Home,
+    Dashboard,
+    BookingRequest,
+    MyAccount,
+    
+}
