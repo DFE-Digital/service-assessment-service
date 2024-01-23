a044cfb62abb21842a7373ac984a0efb73e63589
RogerHowellDfE <96429508+RogerHowellDfE@users.noreply.github.com>
2024-01-23 00:06:24 +0000

    Enable authentication to the service using active directory / entra / microsoft account (#163)
    (cherry picked from commit 80f0a72d561323269d98a84c54f953903d37e1cb)

---
+++ src/ServiceAssessmentService/ServiceAssessmentService.WebApp/Models/GraphUserClient.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Graph;
+using ServiceAssessmentService.WebApp.Pages.Me;
+
+namespace ServiceAssessmentService.WebApp.Models;
+
+public class GraphUserClient
+{
+
+    private readonly GraphServiceClient _graphServiceClient;
+    private readonly ILogger<GraphUserClient> _logger;
+
+    private Microsoft.Graph.User? _graphUser = null;
+
+    public GraphUserClient(GraphServiceClient graphServiceClient, ILogger<GraphUserClient> logger)
+    {
+        _graphServiceClient = graphServiceClient;
+        _logger = logger;
+    }
+
+    public async Task<Microsoft.Graph.User> GetGraphUser()
+    {
+        if (_graphUser == null)
+        {
+            _graphUser = await _graphServiceClient.Me.Request().GetAsync();
+        }
+
+        return _graphUser;
+    }
+
+    public async Task<string> GetGraphUserDisplayName()
+    {
+        var graphUser = await GetGraphUser();
+        return graphUser.DisplayName;
+    }
+
+    public async Task<string> GetGraphUserGivenName()
+    {
+        var graphUser = await GetGraphUser();
+        return graphUser.GivenName;
+    }
+
+    public async Task<string> GetGraphUserSurname()
+    {
+        var graphUser = await GetGraphUser();
+        return graphUser.Surname;
+    }
+
+    public async Task<string> GetGraphUserMail()
+    {
+        var graphUser = await GetGraphUser();
+        return graphUser.Mail;
+    }
+
+}
