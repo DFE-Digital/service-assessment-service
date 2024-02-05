ada54f3e6c5b278052bb9f774659266c2f732cce
Roger Howell <roger.howell@education.gov.uk>
2024-02-05 12:38:42 +0000

    Migrate the delete/remove pages

---
+++ src/ServiceAssessmentService/ServiceAssessmentService.WebApp/Pages/Book/Request/Delete.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ServiceAssessmentService.Application;
+using ServiceAssessmentService.Application.UseCases;
+using ServiceAssessmentService.Domain.Model;
+
+namespace ServiceAssessmentService.WebApp.Pages.Book
+{
+    public class DeletePageModel : PageModel
+    {
+        public AssessmentRequest AssessmentRequest { get; set; }
+
+        private readonly AssessmentRequestRepository _assessmentRequestRepository;
+        private readonly ILogger<DeletePageModel> _logger;
+
+        public DeletePageModel(AssessmentRequestRepository assessmentRequestRepository, ILogger<DeletePageModel> logger)
+        {
+            _assessmentRequestRepository = assessmentRequestRepository;
+            _logger = logger;
+        }
+
+
+        public async Task<IActionResult> OnGet(Guid id)
+        {
+            _logger.LogInformation("Attempting to delete assessment request with ID {Id}", id);
+            var req = await _assessmentRequestRepository.GetByIdAsync(id);
+            if (req == null)
+            {
+                return NotFound();
+            }
+
+            AssessmentRequest = req;
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(Guid id)
+        {
+            var assessmentRequest = await _assessmentRequestRepository.DeleteAsync(id);
+            if (assessmentRequest == null)
+            {
+                _logger.LogInformation("Attempted to delete assessment request with ID {Id}, but it was not found", id);
+                return NotFound();
+            }
+
+            return RedirectToPage("/Book");
+        }
+    }
+}
