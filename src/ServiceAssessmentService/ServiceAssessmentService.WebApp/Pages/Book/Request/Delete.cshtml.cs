87c3684a9cc16448e2fca21f19128c8df352b655
Roger Howell <roger.howell@education.gov.uk>
2024-02-05 13:07:49 +0000

    Tidy out older files, reimplement "create new" functionality, fix links

--- src/ServiceAssessmentService/ServiceAssessmentService.WebApp/Pages/Book/Request/Delete.cshtml.cs
+++ src/ServiceAssessmentService/ServiceAssessmentService.WebApp/Pages/Book/Request/Delete.cshtml.cs
@@ -4,46 +4,45 @@ using ServiceAssessmentService.Application;
 using ServiceAssessmentService.Application.UseCases;
 using ServiceAssessmentService.Domain.Model;
 
-namespace ServiceAssessmentService.WebApp.Pages.Book
+namespace ServiceAssessmentService.WebApp.Pages.Book;
+
+public class DeletePageModel : PageModel
 {
-    public class DeletePageModel : PageModel
+    public AssessmentRequest AssessmentRequest { get; set; }
+
+    private readonly AssessmentRequestRepository _assessmentRequestRepository;
+    private readonly ILogger<DeletePageModel> _logger;
+
+    public DeletePageModel(AssessmentRequestRepository assessmentRequestRepository, ILogger<DeletePageModel> logger)
     {
-        public AssessmentRequest AssessmentRequest { get; set; }
+        _assessmentRequestRepository = assessmentRequestRepository;
+        _logger = logger;
+    }
 
-        private readonly AssessmentRequestRepository _assessmentRequestRepository;
-        private readonly ILogger<DeletePageModel> _logger;
 
-        public DeletePageModel(AssessmentRequestRepository assessmentRequestRepository, ILogger<DeletePageModel> logger)
+    public async Task<IActionResult> OnGet(Guid id)
+    {
+        _logger.LogInformation("Attempting to delete assessment request with ID {Id}", id);
+        var req = await _assessmentRequestRepository.GetByIdAsync(id);
+        if (req == null)
         {
-            _assessmentRequestRepository = assessmentRequestRepository;
-            _logger = logger;
+            return NotFound();
         }
 
+        AssessmentRequest = req;
 
-        public async Task<IActionResult> OnGet(Guid id)
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync(Guid id)
+    {
+        var assessmentRequest = await _assessmentRequestRepository.DeleteAsync(id);
+        if (assessmentRequest == null)
         {
-            _logger.LogInformation("Attempting to delete assessment request with ID {Id}", id);
-            var req = await _assessmentRequestRepository.GetByIdAsync(id);
-            if (req == null)
-            {
-                return NotFound();
-            }
-
-            AssessmentRequest = req;
-
-            return Page();
+            _logger.LogInformation("Attempted to delete assessment request with ID {Id}, but it was not found", id);
+            return NotFound();
         }
 
-        public async Task<IActionResult> OnPostAsync(Guid id)
-        {
-            var assessmentRequest = await _assessmentRequestRepository.DeleteAsync(id);
-            if (assessmentRequest == null)
-            {
-                _logger.LogInformation("Attempted to delete assessment request with ID {Id}, but it was not found", id);
-                return NotFound();
-            }
-
-            return RedirectToPage("/Book");
-        }
+        return RedirectToPage("/Book");
     }
 }
